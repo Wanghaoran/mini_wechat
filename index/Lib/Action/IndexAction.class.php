3bcb41f249eb43730dcfeb4bbdf0512192280440
WangHaoRan <whrlmc@139.com>
2014-04-26 10:48:32 +0800

    sss

---
+++ index/Lib/Action/IndexAction.class.php
@@ -0,0 +1,29 @@
+<?php
+// 本类由系统自动生成，仅供测试用途
+class IndexAction extends Action {
+    public function index(){
+        $result = R('Car/getcar', array(), 'Widget');
+        $this -> assign('result', $result);
+        $this -> display();
+    }
+
+    public function ajaxget(){
+        $id = $_REQUEST['types'];
+        $str = '';
+        for($i=0; $i<5; $i++){
+            $id++;
+            if($id >= 64){
+                continue;
+            }
+            $str .= '<li class="listsss" name="' . $id . '">
+<h4><a><img onclick="clickfun(this);"  src="http://jiacarclub.com/aoxiang_coffee/Index/images/' . $id . '.jpg"/></a></h4>
+                        <img style="display: none;" src="http://jiacarclub.com/aoxiang_coffee/Index/images/' . $id . '_01.jpg"/>
+                        <div class="list-item none">
+                            <img src="http://jiacarclub.com/aoxiang_coffee/Index/images/c' . $id . '.jpg"/>
+                        </div>
+                    </li> ';
+        }
+        echo $str;
+
+    }
+}
