82aab9afbe16319eaa885baaa9a8edc67b09542d
WangHaoRan <whrlmc@139.com>
2014-08-26 18:46:17 +0800

    444

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
+$url="http://mini50.sinaapp.com/";
+header("HTTP/1.1 301 Moved Permanently");
+header ("Location:$url");
+
 define('APP_DEBUG', true);
 define('APP_NAME', 'index');
 define('APP_PATH', './index/');
